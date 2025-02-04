c01622bfee19efbb236f41ddfb2843f602e591cb
Ys_Neptunia <122194676+YsNeptunia@users.noreply.github.com>
2025-02-04 15:13:27 +0800

    Add files via upload

---
+++ LQB15_1/Timer.c
@@ -0,0 +1,20 @@
+#include "Timer.h"
+
+void Timer0_Init(void)		//1毫秒@12.000MHz
+{
+	AUXR &= 0xFB;			//定时器时钟12T模式
+	TMOD &= 0xF0;			//设置定时器模式
+	TMOD |= 0x05;		//GATE = 0,计数器，非自动重装载
+	TL0 = 0;				//设置定时初始值
+	TH0 = 0;				//设置定时初始值
+	TF0 = 0;				//清除TF0标志
+	TR0 = 1;				//定时器0开始计时
+}
+
+void Timer2_Init(void)		//1毫秒@12.000MHz
+{
+	AUXR &= 0xFB;			//定时器时钟12T模式
+	T2L = 0x18;				//设置定时初始值
+	T2H = 0xFC;				//设置定时初始值
+	AUXR |= 0x10;			//定时器2开始计时
+}
